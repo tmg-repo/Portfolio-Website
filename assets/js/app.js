6353e45407d55e8ce766364fa56b8d5c54d47eb1
tmg-repo <98576592+tmg-repo@users.noreply.github.com>
2022-12-29 22:14:32 -0800

    Copying across

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+const scrollUp = document.querySelector("#scroll-up");
+
+scrollUp.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
+
+const burger = document.querySelector("#burger-menu");
+const ul = document.querySelector("nav ul");
+const nav = document.querySelector("nav");
