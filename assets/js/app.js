80f848f089d5e0365bce3d8d67cf412903b3fc57
tmg-repo <98576592+tmg-repo@users.noreply.github.com>
2022-12-30 01:59:26 -0800

    Merging changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,3 +11,15 @@ scrollUp.addEventListener("click", () => {
 const burger = document.querySelector("#burger-menu");
 const ul = document.querySelector("nav ul");
 const nav = document.querySelector("nav");
+
+burger.addEventListener("click", () => {
+    ul.classList.toggle("show");
+  });
+
+  const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((link) =>
+  link.addEventListener("click", () => {
+    ul.classList.remove("show");
+  })
+);
